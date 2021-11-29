d700c50bdf41232e2343374b565c6d966202bc21
Ryan <ryanshahine@msn.com>
2021-11-29 21:33:27 +0100

    First upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeUrl(){
+let betaurl = "https://tc9wdh8rhe9.typeform.com/to/dvSk3u6x#email="
+   let email = document.querySelector("#cta-email").value
+    window.location = betaurl+email;    
+}
+
