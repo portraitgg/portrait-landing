b235bb54b530e2d2db4dfa2e5c2947bd66e7369f
Giliam <verheide.giliam@gmail.com>
2022-03-22 12:46:05 +0100

    feat: Update favicons with new logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,13 +10,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b1f1" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
+          <link rel="manifest" href="/site.webmanifest?v=2" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#3d3d3d" />
+          <link rel="shortcut icon" href="/favicon.ico?v=2" />
+          <meta name="apple-mobile-web-app-title" content="Portrait" />
+          <meta name="application-name" content="Portrait" />
+          <meta name="msapplication-TileColor" content="#8e8e8e" />
+          <meta name="theme-color" content="#000000" />
         </Head>
         <body className="bg-gray-1000 antialiased">
           <Main />
