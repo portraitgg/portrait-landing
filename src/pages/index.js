1e9ac200e6e43179d45b7680b6a6bf34da17ced3
Giliam <verheide.giliam@gmail.com>
2022-06-09 10:07:44 -0500

    Added QR code invite notification

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ TextBlock.defaultProps = {
 
 export default function Home() {
   const router = useRouter()
-  const { invite } = router.query
+  const { invite, qr } = router.query
 
   useEffect(() => {
     if (invite === 'consensus2022') {
@@ -100,7 +100,43 @@ export default function Home() {
         }
       )
     }
-  }, [invite])
+
+    if (qr !== undefined) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`${
+              t.visible ? styles['animate-enter'] : styles['animate-leave']
+            } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 transform`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start">
+                <div className="flex-shrink-0 pt-0.5 text-xl">👋</div>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">You scanned our QR code..</p>
+                  <p className="mt-0.5 text-sm text-gray-500">
+                    ..and received an invite to join the Portrait beta!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                type="button"
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        {
+          duration: 20000
+        }
+      )
+    }
+  }, [invite, qr])
 
   return (
     <div className="overflow-hidden">
